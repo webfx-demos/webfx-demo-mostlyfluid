dfe67f0a03a9efd62d0b8e375d86240b8d207570
Bruno Salmon <dev.salmonb@gmail.com>
2021-03-04 10:06:20 +0000

    2 mostly-fluid panes in a split pane

--- webfx-demo-responsivedesign-application/src/main/java/dev/webfx/demo/responsivedesign/ResponsiveDesignApplication.java
+++ webfx-demo-responsivedesign-application/src/main/java/dev/webfx/demo/responsivedesign/ResponsiveDesignApplication.java
@@ -10,7 +10,6 @@ import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
 import javafx.stage.Stage;
 
 /**
@@ -20,44 +19,45 @@ public class ResponsiveDesignApplication extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        SplitPane horizontalScrollPane = new SplitPane(createFluidPane(), createArea(Color.PURPLE));
-        //SplitPane verticalSplitPane    = new SplitPane(horizontalScrollPane, new TallyCounterApplication().createTallyCounterPane());
-        //verticalSplitPane.setOrientation(Orientation.VERTICAL);
-        primaryStage.setScene(new Scene(horizontalScrollPane, 850, 450));
+        primaryStage.setScene(new Scene(new SplitPane(createMostlyFluidPane(0), createMostlyFluidPane(100)), 850, 450));
         primaryStage.setTitle("Responsive Design");
         primaryStage.show();
     }
 
-    private static Pane createFluidPane() {
-        Region a1 = createArea(Color.rgb(0, 52, 118));
-        Region a2 = createArea(Color.rgb(0, 98, 210));
-        Region a3 = createArea(Color.rgb(180, 210, 247));
-        Region a4 = createArea(Color.rgb(213, 223, 239));
-        Region a5 = createArea(Color.rgb(223, 225, 229));
+    private static Pane createMostlyFluidPane(double hueShift) {
+        Region a1 = createArea(Color.rgb(0, 52, 118), hueShift);
+        Region a2 = createArea(Color.rgb(0, 98, 210), hueShift);
+        Region a3 = createArea(Color.rgb(180, 210, 247), hueShift);
+        Region a4 = createArea(Color.rgb(213, 223, 239), hueShift);
+        Region a5 = createArea(Color.rgb(223, 225, 229), hueShift);
         return new Pane(a1, a2, a3, a4, a5) {
             @Override
             protected void layoutChildren() {
-                double width = getWidth();
+                double width = getWidth(), x1, y1, w1, h1, x2, y2, w2, h2, x3, y3, w3, h3, x4, y4, w4, h4, x5, y5, w5, h5;
                 if (width >= 800) {
-                    double left = (width - 800) / 2;
-                    layoutInArea(a1, left, 0, 480, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a2, left + 480, 0, 320, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a3, left, 150, 264, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a4, left + 264, 150, 264, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a5, left + 264*2, 150, 272, 150, 0, HPos.LEFT, VPos.TOP);
+                    x1 = (width - 800) / 2;  y1 = 0;         w1 = 0.6  * 800;    h1 = 150;
+                    x2 = x1 + w1;            y2 = 0;         w2 = 0.4  * 800;    h2 = 150;
+                    x3 = x1;                 y3 = y1 + h1;   w3 = 0.33 * 800;    h3 = 150;
+                    x4 = x3 + w3;            y4 = y3;        w4 = 0.33 * 800;    h4 = 150;
+                    x5 = x4 + w4;            y5 = y3;        w5 = 0.34 * 800;    h5 = 150;
                 } else if (width > 600) {
-                    layoutInArea(a1, 0, 0, width, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a2, 0, 150, width / 2, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a3, width / 2, 150, width / 2, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a4, 0, 300, width / 2, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a5, width / 2, 300, width / 2, 150, 0, HPos.LEFT, VPos.TOP);
+                    x1 = 0;                  y1 = 0;         w1 = width;         h1 = 150;
+                    x2 = 0;                  y2 = y1 + h1;   w2 = 0.5 * width;   h2 = 150;
+                    x3 = width / 2;          y3 = y2;        w3 = 0.5 * width;   h3 = 150;
+                    x4 = 0;                  y4 = y3 + h3;   w4 = 0.5 * width;   h4 = 150;
+                    x5 = width / 2;          y5 = y4;        w5 = 0.5 * width;   h5 = 150;
                 } else {
-                    layoutInArea(a1, 0, 0, width, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a2, 0, 150, width, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a3, 0, 300, width, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a4, 0, 450, width, 150, 0, HPos.LEFT, VPos.TOP);
-                    layoutInArea(a5, 0, 600, width, 150, 0, HPos.LEFT, VPos.TOP);
+                    x1 = 0;                  y1 = 0;         w1 = width;         h1 = 150;
+                    x2 = 0;                  y2 = y1 + h1;   w2 = width;         h2 = 150;
+                    x3 = 0;                  y3 = y2 + h2;   w3 = width;         h3 = 150;
+                    x4 = 0;                  y4 = y3 + h3;   w4 = width;         h4 = 150;
+                    x5 = 0;                  y5 = y4 + h4;   w5 = width;         h5 = 150;
                 }
+                layoutInArea(a1, x1, y1, w1, h1, 0, HPos.LEFT, VPos.TOP);
+                layoutInArea(a2, x2, y2, w2, h2, 0, HPos.LEFT, VPos.TOP);
+                layoutInArea(a3, x3, y3, w3, h3, 0, HPos.LEFT, VPos.TOP);
+                layoutInArea(a4, x4, y4, w4, h4, 0, HPos.LEFT, VPos.TOP);
+                layoutInArea(a5, x5, y5, w5, h5, 0, HPos.LEFT, VPos.TOP);
             }
 
             @Override
@@ -69,9 +69,9 @@ public class ResponsiveDesignApplication extends Application {
         };
     }
 
-    private static Region createArea(Paint fill) {
+    private static Region createArea(Color color, double hueShift) {
         Region area = new Region();
-        area.setBackground(new Background(new BackgroundFill(fill, null, null)));
+        area.setBackground(new Background(new BackgroundFill(color.deriveColor(hueShift, 1, 1, 1), null, null)));
         return area;
     }
 }
