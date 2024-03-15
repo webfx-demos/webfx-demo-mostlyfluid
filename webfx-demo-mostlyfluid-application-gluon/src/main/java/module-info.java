f39aa1d78a5854a98968f0a21f1bbfecb2b7bf39
Bruno Salmon <dev.salmonb@gmail.com>
2024-03-15 08:28:43 +0000

    Added j2cl module

--- webfx-demo-mostlyfluid-application-gluon/src/main/java/module-info.java
+++ webfx-demo-mostlyfluid-application-gluon/src/main/java/module-info.java
@@ -4,15 +4,12 @@ module webfx.demo.mostlyfluid.application.gluon {
 
     // Direct dependencies modules
     requires webfx.demo.mostlyfluid.application;
-    requires webfx.kit.openjfx;
+    requires webfx.kit.javafxgraphics.openjfx;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
     requires webfx.platform.os.gluon;
     requires webfx.platform.resource.gluon;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.gluon;
-
-    // Resources packages
-    opens dev.webfx.platform.meta.exe;
 
 }
--- webfx-demo-mostlyfluid-application-openjfx/src/main/java/module-info.java
+++ webfx-demo-mostlyfluid-application-openjfx/src/main/java/module-info.java
@@ -4,14 +4,12 @@ module webfx.demo.mostlyfluid.application.openjfx {
 
     // Direct dependencies modules
     requires webfx.demo.mostlyfluid.application;
-    requires webfx.kit.openjfx;
+    requires webfx.kit.javafxgraphics.openjfx;
     requires webfx.platform.boot.java;
     requires webfx.platform.console.java;
+    requires webfx.platform.os.java;
     requires webfx.platform.resource.java;
     requires webfx.platform.scheduler.java;
     requires webfx.platform.shutdown.java;
-
-    // Resources packages
-    opens dev.webfx.platform.meta.exe;
 
 }
